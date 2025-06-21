a6480733ba042f2f064d8fd46037ed49c0a7a931
Meshack2213 <meshackzkl@gmail.com>
2025-06-21 10:42:32 +0300

    new file:   lib/utils/dateUtils.ts

---
+++ lib/utils/dateUtils.ts
@@ -0,0 +1,20 @@
+export function timeExpenseAdded (timestamp:string){
+
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString("en-US",{
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,       
+    });
+
+}
+
+export function formatDateHeader(dateString: string) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
